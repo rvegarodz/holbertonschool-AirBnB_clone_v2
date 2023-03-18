c7a2c2bb5b8d592d766adaf45e19e515d2b8d2b6
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-18 12:22:14 -0400

    setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Script that prepares MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
