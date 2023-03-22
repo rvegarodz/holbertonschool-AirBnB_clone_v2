c5467374b605bba4b8cfd42736474cce46bd6051
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-03-22 14:59:59 -0400

    Committing local changes before pulling from remote repository

--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.state import State
+from sqlalchemy.orm import relationship
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey(State.id))
+    name = Column(String(128))
+
+places = relationship("Place", cascade="delete", backref="cities")
