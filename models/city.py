60e8be2c12f47dd9d9c99219c24f24ac46c1e6a3
rvegarodz <r.vega27@outlook.com>
2023-03-22 15:54:39 -0400

    restoring city class

--- models/city.py
+++ models/city.py
@@ -8,3 +8,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
