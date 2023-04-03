da572c9fbd273295e22364f377eb707ce5eb703d
rvegarodz <r.vega27@outlook.com>
2023-04-03 10:52:35 -0400

    adding file edited

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+ #!/usr/bin/python3
+"""This module contain a Flask instance"""
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """Function that starts a Flask web application"""
     return 'Hello HBNB!'
