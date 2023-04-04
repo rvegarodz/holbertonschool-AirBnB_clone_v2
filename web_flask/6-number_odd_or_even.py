c547413cee0af026d68e6430f646327e65a4eea5
rvegarodz <r.vega27@outlook.com>
2023-04-04 10:55:19 -0400

    adding file for task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This module contain a Flask instance"""
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function that starts a Flask web application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_2():
+    """Function that starts a Flask web application"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_3(text):
+    """Function that starts a Flask web application"""
+    return 'C ' + f'{escape(text)}'.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def hello_4(text):
+    """Function that starts a Flask web application"""
+    return 'Python ' + f'{escape(text)}'.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_5(n):
+    """Function that starts a Flask web application"""
+    if type(n) == int:
+        return f'{escape(n)}' + ' is a number'
+
+
+@app.route('/number_template/<int:n>')
+def hello_6(n=None):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def hello_7(n=None):
+    result = ""
+    if (n % 2) == 0:
+        result = "even"
+    else:
+        result = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, result=result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
