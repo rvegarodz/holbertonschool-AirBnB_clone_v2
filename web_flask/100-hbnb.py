a336e927e7a25163e308a06a549237de553b702b
rvegarodz <r.vega27@outlook.com>
2023-04-08 15:55:01 -0400

    adding files for advace task (not finish)

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+"""This is the module documentation"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+"""
+Script that starts a Flask
+Web app for my HBNB clone project
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return "C " + f'{text}'.replace("_", " ")
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    if text == "is cool":
+        return "Python is cool"
+    else:
+        return "Python " + f'{text}'.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    all_states = storage.all(State).values()
+    return render_template('7-states_list.html',
+                           all_states=all_states, header="States")
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
+    all_states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', all_states=all_states,
+                           header="States")
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states(id=None):
+    if id:
+        try:
+            state = storage.all(State)[f'State.{id}']
+            return render_template('9-states.html', state=state, id=id)
+        except KeyError:
+            return render_template('9-states.html', error="Error",)
+    else:
+        all_states = storage.all(State).values()
+        return render_template('9-states.html',
+                               all_states=all_states)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filters():
+    all_states = storage.all(State).values()
+    all_amenities = storage.all(Amenity).values()
+    return (render_template('10-hbnb_filters.html',
+                            all_states=all_states,
+                            all_amenities=all_amenities))
+
+
+@app.route("/hbnb", strict_slashes=False)
+def places():
+    all_states = storage.all(State).values()
+    all_amenities = storage.all(Amenity).values()
+    all_places = storage.all(Place).values()
+    return (render_template('100-hbnb.html',
+                            all_states=all_states,
+                            all_amenities=all_amenities,
+                            all_places=all_places))
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
